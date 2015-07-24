ca96ebb6f2a8aa940f1b78fd85bbf9088ec03ef5
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-24 09:26:50 +0000

    completed tournament implementation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # yolo-bear-server-nodejs
-Nodejs version of yolo-bear to be run on aws lambda instead of aws ec2
+Nodejs version of [yolo-bear-server](https://github.com/shadiakiki1986/yolo-bear-server) (which was in php).
+This nodejs version can run on aws lambda instead of aws ec2
--- test/TournamentTest.js
+++ test/TournamentTest.js
@@ -1,11 +1,35 @@
 'use strict';
-var YoloBearTournament = require('app/YoloBearTournament');
+var Tournament = require('app/Tournament');
 var should = require('should');
 
-describe('YoloBearTournament tests', function() {
+describe('Tournament tests', function() {
+
+  it('get', function(done) {
+    var dm = new Tournament();
+    dm.get(
+      "some random name",
+      { fail:function(err) { should.fail('Error: '+err);},
+        succeed:function(data,res) {
+          res.should.eql(false);
+          done();
+        }
+      }
+    );
+  });
+
+  it('list', function(done) {
+    var dm = new Tournament();
+    dm.list({
+      fail:function(err) { should.fail('Error: '+err);},
+      succeed:function(data) {
+        data.length.should.above(-1); // not sure how to test this
+        done();
+      }
+    });
+  });
 
   it('cycle: drop, not exists, get, exists', function(done) {
-    var dm = new YoloBearTournament();
+    var dm = new Tournament();
     var mycontinue = function() {
       dm.get(
         "test 123",
@@ -24,7 +48,7 @@ describe('YoloBearTournament tests', function() {
                       succeed:function(data,res) {
                         res.should.eql(true);
 
-                        dm.drop("test 123",{
+                        dm.drop("test 123","pass123",{
                           fail:function(err) { should.fail("Shouldnt get here"); },
                           succeed:function() { done(); }
                         });
@@ -37,23 +61,12 @@ describe('YoloBearTournament tests', function() {
         });
     };
 
-    dm.drop("test 123",{
+    dm.drop("test 123","pass123",{
       fail:function(err) { mycontinue(); }, // silent fail on not exists
       succeed:function() { mycontinue(); }
     });
   });
 
-  it('list', function(done) {
-    var dm = new YoloBearTournament();
-    dm.list({
-      fail:function(err) { should.fail('Error: '+err);},
-      succeed:function(data) {
-        data.length.should.above(-1); // not sure how to test this
-        done();
-      }
-    });
-  });
 
 }); // end describe
 
-});
